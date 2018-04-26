0b85dad555f8ad776e77ea44f596f1255e568e45
Soontrax <32308953+Soontrax@users.noreply.github.com>
2018-04-26 20:39:18 +0200

    Add files via upload

--- src/jdbc/Principal.java
+++ src/jdbc/Principal.java
@@ -15,34 +15,39 @@ import javax.swing.JOptionPane;
  * @author infor05
  */
 public class Principal {
+
     public static void main(String[] args) {
         try {
             boolean salir = false;
-            Principal menus = null;
             Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/beer", "root", "12345");
-            while(!salir){
+            while (!salir) {
                 JOptionPane.showMessageDialog(null, "Bienvenido al Gestor de base de datos del IES Francesc Borja Moll");
                 System.out.println("1. Consultar la Base de Datos");
-                System.out.println("2. Actualizar la Base de Datos");
-                System.out.println("3. Insertar datos a la Base de Datos");
+                System.out.println("2. Actualizar la Base de Datos(UPDATE)");
+                System.out.println("3. Insertar datos a la Base de Datos(INSERT)");
                 System.out.println("4. Salir");
                 int opcion = Integer.parseInt(JOptionPane.showInputDialog("Que opción quieres"));
-                switch (opcion){
+                switch (opcion) {
                     case 1:
-                        menus.ConsultarBasedeDatos(conexion);
+                        Principal.ConsultarBasedeDatos(conexion);
+                        break;
                     case 2:
-                        menus.ActualizarBasedeDatos(conexion);
+                        Principal.ActualizarBasedeDatos(conexion);
+                        break;
                     case 3:
-                        menus.InsertardatosBasedeDatos(conexion);
+                        Principal.InsertardatosBasedeDatos(conexion);
+                        break;
                     case 4:
                         salir = true;
                         JOptionPane.showMessageDialog(null, "Que pases un buen dia");
                 }
-            }   } catch (SQLException ex) {
-            System.out.println("NO CONECTA");
+            }
+        } catch (SQLException ex) {
+            System.out.println("La conexión no se ha establecido correctamente");
         }
-}
-    public void ConsultarBasedeDatos(Connection conexion){
+    }
+
+    public static void ConsultarBasedeDatos(Connection conexion) {
         try {
             JOptionPane.showMessageDialog(null, "Has accedido a la opción Consultar Base de Datos");
             ResultSet resultado;
@@ -50,22 +55,33 @@ public class Principal {
                 resultado = st.executeQuery("SELECT * FROM bar");
                 String nombre;
                 String direccion;
-                while (resultado.next()){
+                while (resultado.next()) {
                     nombre = resultado.getString("name");
                     direccion = resultado.getString("address");
+                    System.out.println(nombre);
+                    System.out.println(direccion);
                 }
             }
             resultado.close();
         } catch (SQLException ex) {
             Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, "La consulta no se ha hayado correctamente");
         }
+
     }
-    
-    public void ActualizarBasedeDatos(Connection conexion){
+
+    public static void ActualizarBasedeDatos(Connection conexion) {
         JOptionPane.showMessageDialog(null, "Has accedido a la opción Actualizar Base de Datos");
+        ResultSet resultado;
+        try {
+            Statement st = conexion.createStatement();
+            int rowaf = st.executeUpdate("UPDATE bar SET name = 'Pepe' WHERE name = 'Satisfaction'");
+            System.out.println("El numero de filas afectadas ha sido" + rowaf);
+        } catch (SQLException ex) {
+            Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
-    public void InsertardatosBasedeDatos(Connection conexion){
+
+    public static void InsertardatosBasedeDatos(Connection conexion) {
         JOptionPane.showMessageDialog(null, "Has accedido a la opción Insertar Datos");
     }
 }
